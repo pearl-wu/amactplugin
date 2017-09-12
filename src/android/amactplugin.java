3a0aa6d17ccd299bd41ebd8e9cee038e92257d0e
pearl <lu92500@yahoo.com.tw>
2017-09-12 16:20:48 +0800

    Update amactplugin.java

--- src/android/amactplugin.java
+++ src/android/amactplugin.java
@@ -1,13 +1,18 @@
 package com.bais.amactplugin;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.Uri;
+import android.util.Log;
+import android.webkit.CookieManager;
 import android.widget.Toast;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.LOG;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -16,6 +21,9 @@ import org.json.JSONObject;
 public class amactplugin extends CordovaPlugin {
     private CallbackContext callbackContext;
     private JSONObject params;
+    private static final String LOG_TAG = "WebViewPlugin";
+    private static CallbackContext subscribeCallbackContext = null;
+    public static final int REQUEST_CODE = 0;
 
     @Override
     public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException {
@@ -56,9 +64,16 @@ public class amactplugin extends CordovaPlugin {
           }else if(action.equals("openwebshow")){
               this.params = args.getJSONObject(0);
               String webUrl = params.getString("url");
-              callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK));
-              //Toast.makeText(this.cordova.getActivity(),webUrl,Toast.LENGTH_SHORT).show(); 
-             this.cordova.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); 
+              //this.callbackContext.success("OK");
+              //Toast.makeText(this.cordova.getActivity(),webUrl,Toast.LENGTH_SHORT).show();
+             this.cordova.getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+              Boolean shouldShowLoading = false;
+              try{
+                  shouldShowLoading = args.getBoolean(1);
+              }
+              catch(Exception e){
+
+              }
              if(!"".equals(webUrl)) {
                 showWebView(webUrl, shouldShowLoading);
                 JSONObject r = new JSONObject();
@@ -72,7 +87,14 @@ public class amactplugin extends CordovaPlugin {
               JSONObject r = new JSONObject();
               r.put("responseCode", "ok");
               this.callbackContext.success(r);
+          }else if(action.equals("cookie-get")){
+              CookieManager.setAcceptFileSchemeCookies(true); //available in android level 12
+              CookieManager.getInstance().setAcceptCookie(true); //available in android level 12
+                Log.d("PHPGUID: ", CookieManager.getInstance().getCookie("PHPGUID"));
+                Log.d("MjMtZA%3D%3D: ", CookieManager.getInstance().getCookie("MjMtZA%3D%3D"));
+                Log.d("MjMtcw%3D%3D: ", CookieManager.getInstance().getCookie("MjMtcw%3D%3D"));
             }
+
         return false;
      }
     
@@ -82,7 +104,8 @@ public class amactplugin extends CordovaPlugin {
         i.putExtra("url", url);
         i.putExtra("shouldShowLoading", shouldShowLoading);
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        this.cordova.getActivity().getApplicationContext().startActivity(i);
+        this.cordova.getActivity().startActivityForResult(i, REQUEST_CODE);
+        //this.cordova.getActivity().getApplicationContext().startActivity(i);
       }
 
       private void hideWebView() {
@@ -94,4 +117,13 @@ public class amactplugin extends CordovaPlugin {
           subscribeCallbackContext = null;
         }
       }
+    @Override
+     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        LOG.d("..............................................", "Calling subscribeCallbackContext success");
+        if(resultCode== Activity.RESULT_OK){
+            String key=intent.getStringExtra("key");
+            Toast.makeText(this.cordova.getActivity(),key,Toast.LENGTH_SHORT).show();
+            this.callbackContext.success(key);
+        }
+     }
 }
