fc63b413beadfbea9d28656f95a6e92b25c87821
pearl <lu92500@yahoo.com.tw>
2018-03-06 10:28:52 +0800

    Update amactplugin.js

--- www/amactplugin.js
+++ www/amactplugin.js
@@ -1,37 +1,32 @@
 var exec = require('cordova/exec');
 
-exports.version = function(win, fail){
-    exec(win, fail, "amactplugin", "version", []);
-}
 
-exports.openapp = function(message, win, fail){
-    exec(win, fail, "amactplugin", "openapp", [message]);
-}
-
-exports.openweb = function(message, win, fail){
-    exec(win, fail, "amactplugin", "openweb", [message]);
-}
-
-exports.openwebshow = function(message, win, fail){
-    exec(win, fail, "amactplugin", "openwebshow", [message]);
-}
-
-exports.openwebclose = function(win, fail){
-    exec(win, fail, "amactplugin", "openwebclose", []);
-}
-
-exports.getcookie = function(message,win, fail){
-    exec(win, fail, "amactplugin", "cookieget", [message]);
-}
-
-exports.setcookie = function(message,win, fail){
-    exec(win, fail, "amactplugin", "cookieset", [message]);
-}
-
-exports.clearcookie = function(win, fail){
-    exec(win, fail, "amactplugin", "cookieclear", []);
-}
-
-exports.encode = function(message,win, fail){
-    exec(win, fail, "amactplugin", "encode", [message]);
+module.exports = {
+    version : function(win, fail){
+        exec(win, fail, "amactplugin", "version", []);
+    },
+    openapp: function(message, win, fail){
+        exec(win, fail, "amactplugin", "openapp", [message]);
+    },
+    openweb: function(message, win, fail){
+        exec(win, fail, "amactplugin", "openweb", [message]);
+    },
+    openwebshow: function(message, win, fail){
+        exec(win, fail, "amactplugin", "openwebshow", [message]);
+    },
+    openwebclose: function(win, fail){
+        exec(win, fail, "amactplugin", "openwebclose", []);
+    },
+    getcookie: function(message,win, fail){
+        exec(win, fail, "amactplugin", "cookieget", [message]);
+    },
+    setcookie: function(message,win, fail){
+        exec(win, fail, "amactplugin", "cookieset", [message]);
+    },
+    clearcookie: function(win, fail){
+        exec(win, fail, "amactplugin", "cookieclear", []);
+    },
+    encode: function(message,win, fail){
+        exec(win, fail, "amactplugin", "encode", [message]);
+    }
 }
