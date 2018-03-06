e84bbeaf2ee64e7260ffca8994b827c040a0f099
pearl <lu92500@yahoo.com.tw>
2018-03-06 10:31:32 +0800

    Update amactplugin.js

--- www/amactplugin.js
+++ www/amactplugin.js
@@ -1,30 +1,12 @@
 var exec = require('cordova/exec');
 module.exports = {
-    version : function (win, fail){
-        exec(win, fail, "amactplugin", "version", []);
-    },
-    openapp: function (message, win, fail){
-        exec(win, fail, "amactplugin", "openapp", [message]);
-    },
-    openweb: function (message, win, fail){
-        exec(win, fail, "amactplugin", "openweb", [message]);
-    },
-    openwebshow: function (message, win, fail){
-        exec(win, fail, "amactplugin", "openwebshow", [message]);
-    },
-    openwebclose: function (win, fail){
-        exec(win, fail, "amactplugin", "openwebclose", []);
-    },
-    getcookie: function (message,win, fail){
-        exec(win, fail, "amactplugin", "cookieget", [message]);
-    },
-    setcookie: function (message,win, fail){
-        exec(win, fail, "amactplugin", "cookieset", [message]);
-    },
-    clearcookie: function (win, fail){
-        exec(win, fail, "amactplugin", "cookieclear", []);
-    },
-    encode: function (message,win, fail){
-        exec(win, fail, "amactplugin", "encode", [message]);
-    }
+    version: function (win, fail){ exec(win, fail, "amactplugin", "version", []); },
+    openapp: function (message, win, fail){ exec(win, fail, "amactplugin", "openapp", [message]); },
+    openweb: function (message, win, fail){ exec(win, fail, "amactplugin", "openweb", [message]); },
+    openwebshow: function (message, win, fail){ exec(win, fail, "amactplugin", "openwebshow", [message]); },
+    openwebclose: function (win, fail){ exec(win, fail, "amactplugin", "openwebclose", []); },
+    getcookie: function (message,win, fail){ exec(win, fail, "amactplugin", "cookieget", [message]); },
+    setcookie: function (message,win, fail){ exec(win, fail, "amactplugin", "cookieset", [message]); },
+    clearcookie: function (win, fail){ exec(win, fail, "amactplugin", "cookieclear", []); },
+    encode: function (message,win, fail){ exec(win, fail, "amactplugin", "encode", [message]); }
 };
